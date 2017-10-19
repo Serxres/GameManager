7314e5ae6dd7fe8abaa589b445318f8db89f4c8b
sergi gonzalez <serxres@hotmail.com>
2017-10-19 13:18:05 +0200

    time, animacio etc...

--- src/A05/A05Base.cpp
+++ src/A05/A05Base.cpp
@@ -2,10 +2,14 @@
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 #include <SDL_mixer.h>
+#include <iostream>
+#include <time.h>
 
 //Game general information
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+
+#define FPS 60
 
 int main(int, char*[]) {
 
@@ -16,6 +20,13 @@ int main(int, char*[]) {
 	if (TTF_Init() != 0) throw "No es pot inicialitzar SDL_ttf";
 	const Uint8 mixFlags{ MIX_INIT_MP3 | MIX_INIT_OGG };
 	if (!(Mix_Init(mixFlags) & mixFlags)) throw "Error:SDL_mixer init";
+
+
+	// --- TIME ---
+	clock_t lastTime = clock();
+	float timeDown = 10.;
+	float deltaTime = 0;
+
 
 	// --- WINDOW ---
 	SDL_Window *window{ SDL_CreateWindow("SDL...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN) };
@@ -30,11 +41,23 @@ int main(int, char*[]) {
 	if (bgTexture == nullptr)throw "No s'han pogut crear les textures";
 	SDL_Rect bgRect{ 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
 
-	SDL_Texture *playerTexture{ IMG_LoadTexture(renderer, "../../res/img/kintoun.png") };
-	if (playerTexture == nullptr) throw "Nos'han pogut crear les textures";
-	SDL_Rect playerRect{ 0, 0, 350, 189 };
+	//SDL_Texture *playerTexture{ IMG_LoadTexture(renderer, "../../res/img/kintoun.png") };
+	//if (playerTexture == nullptr) throw "Nos'han pogut crear les textures";
+	//SDL_Rect playerRect{ 0, 0, 350, 189 };
 	SDL_Rect playerTarget{ 0, 0, 100, 100 };
 		// --- Animated Sprite ---
+
+	SDL_Texture *playerTexture{ IMG_LoadTexture(renderer, "../../res/img/sp01.png") };
+	SDL_Rect playerRect, playerPosition;
+	int textWidth, textHeight, frameWidth, frameHeight;
+	SDL_QueryTexture(playerTexture, NULL, NULL, &textWidth, &textHeight);
+	frameWidth = textWidth / 6;
+	frameHeight = textHeight / 1;
+	playerPosition.x = playerPosition.y = 0;
+	playerRect.x = playerRect.y = 0;
+	playerPosition.h = playerRect.h = frameHeight;
+	playerPosition.w = playerRect.w = frameWidth;
+	int frameTime = 0;
 
 	// --- TEXT ---
 	TTF_Font *font{ TTF_OpenFont("../../res/ttf/saiyan.ttf", 80) };
@@ -76,21 +99,38 @@ int main(int, char*[]) {
 		}
 
 		// UPDATE
-		playerRect.x += (playerTarget.x - playerRect.x) / 10;
-		playerRect.y += (playerTarget.y - playerRect.y) / 10;
+		//playerRect.x += (playerTarget.x - playerRect.x) / 10;
+		//playerRect.y += (playerTarget.y - playerRect.y) / 10;
+
+		deltaTime = (clock() - lastTime);
+		lastTime = clock();
+		deltaTime /= CLOCKS_PER_SEC;
+		timeDown -= deltaTime;
+		std::cout << timeDown << std::endl;
+
+
+		frameTime++;
+		if (FPS / frameTime <= 9)
+		{
+			frameTime = 0;
+			playerRect.x += frameWidth;
+			if (playerRect.x >= textWidth)
+				playerRect.x = 0;
+		}
 
 
 		// DRAW
 		SDL_RenderClear(renderer);
 		SDL_RenderCopy(renderer, bgTexture, nullptr, &bgRect);
 		SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
-		SDL_RenderCopy(renderer, playerTexture, nullptr, &playerRect);
-		SDL_RenderPresent(renderer);
+		//SDL_RenderCopy(renderer, playerTexture, nullptr, &playerRect);
+		//SDL_RenderPresent(renderer);
 			//Background
 		
 			//Animated Sprite
+		
+		SDL_RenderCopy(renderer, playerTexture, &playerRect, &playerPosition);
 		SDL_RenderPresent(renderer);
-
 	}
 
 	// --- DESTROY ---
