eb8492a0605b5309c6ac8878bbc1033fe5fbfa64
sergi gonzalez <serxres@hotmail.com>
2017-10-24 13:27:29 +0200

    Mejoras

--- src/A05/A05Base.cpp
+++ src/A05/A05Base.cpp
@@ -2,8 +2,8 @@
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 #include <SDL_mixer.h>
-#include <iostream>
 #include <time.h>
+#include <iostream>
 
 //Game general information
 #define SCREEN_WIDTH 800
@@ -15,18 +15,15 @@ int main(int, char*[]) {
 
 	// --- INIT ---
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) throw "No es pot inicialitzar SDL subsystems";
-	const Uint8 imgFlags {IMG_INIT_PNG | IMG_INIT_JPG };
+	const Uint8 imgFlags{ IMG_INIT_PNG | IMG_INIT_JPG };
 	if (!(IMG_Init(imgFlags) & imgFlags))throw "Error al carregar!";
 	if (TTF_Init() != 0) throw "No es pot inicialitzar SDL_ttf";
 	const Uint8 mixFlags{ MIX_INIT_MP3 | MIX_INIT_OGG };
 	if (!(Mix_Init(mixFlags) & mixFlags)) throw "Error:SDL_mixer init";
-
-
-	// --- TIME ---
+	// ---TIIME---
 	clock_t lastTime = clock();
-	float timeDown = 10.;
+	float timeDown = 10;
 	float deltaTime = 0;
-
 
 	// --- WINDOW ---
 	SDL_Window *window{ SDL_CreateWindow("SDL...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN) };
@@ -37,7 +34,7 @@ int main(int, char*[]) {
 	if (renderer == nullptr) throw "No es pot inicialitzar SDL_Renderer";
 
 	// --- SPRITES ---
-	SDL_Texture *bgTexture{ IMG_LoadTexture(renderer, "../../res/img/bg.jpg") };
+	SDL_Texture *bgTexture{ IMG_LoadTexture(renderer, "../../res/img/bgCastle.jpg") };
 	if (bgTexture == nullptr)throw "No s'han pogut crear les textures";
 	SDL_Rect bgRect{ 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
 
@@ -45,15 +42,16 @@ int main(int, char*[]) {
 	//if (playerTexture == nullptr) throw "Nos'han pogut crear les textures";
 	//SDL_Rect playerRect{ 0, 0, 350, 189 };
 	SDL_Rect playerTarget{ 0, 0, 100, 100 };
-		// --- Animated Sprite ---
+	// --- Animated Sprite ---
 
-	SDL_Texture *playerTexture{ IMG_LoadTexture(renderer, "../../res/img/sp01.png") };
+	SDL_Texture *playerTexture{ IMG_LoadTexture(renderer, "../../res/img/spCastle.png") };
 	SDL_Rect playerRect, playerPosition;
 	int textWidth, textHeight, frameWidth, frameHeight;
 	SDL_QueryTexture(playerTexture, NULL, NULL, &textWidth, &textHeight);
-	frameWidth = textWidth / 6;
-	frameHeight = textHeight / 1;
-	playerPosition.x = playerPosition.y = 0;
+	frameWidth = textWidth / 12;
+	frameHeight = textHeight / 8;
+	playerPosition.x = SCREEN_WIDTH / 2;
+	playerPosition.y = SCREEN_HEIGHT / 2;
 	playerRect.x = playerRect.y = 0;
 	playerPosition.h = playerRect.h = frameHeight;
 	playerPosition.w = playerRect.w = frameWidth;
@@ -62,13 +60,13 @@ int main(int, char*[]) {
 	// --- TEXT ---
 	TTF_Font *font{ TTF_OpenFont("../../res/ttf/saiyan.ttf", 80) };
 	if (font == nullptr) throw "No es pot iicialitzar the TTF_Font";
-	SDL_Surface *tmpSurf{ TTF_RenderText_Blended(font, "My first SDL game", SDL_Color {255, 150, 0, 255}) };
+	SDL_Surface *tmpSurf{ TTF_RenderText_Blended(font, "My first SDL game", SDL_Color{ 255, 150, 0, 255 }) };
 	if (tmpSurf == nullptr) TTF_CloseFont(font), throw "Unable to create the SDL text surface";
 	SDL_Texture *textTexture{ SDL_CreateTextureFromSurface(renderer, tmpSurf) };
 	SDL_Rect textRect{ 100, 50, tmpSurf->w, tmpSurf->h };
 	SDL_FreeSurface(tmpSurf);
 	TTF_CloseFont(font);
-	
+
 	// --- AUDIO ---
 	if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 1024) == -1)
 	{
@@ -82,16 +80,22 @@ int main(int, char*[]) {
 	// --- GAME LOOP ---
 	SDL_Event event;
 	bool isRunning = true;
-	while (isRunning){
+	while (isRunning) {
 		// HANDLE EVENTS
 		while (SDL_PollEvent(&event)) {
-			switch (event.type) {
+			switch (event.key.keysym.sym) {
 			case SDL_QUIT:		isRunning = false; break;
-			case SDL_MOUSEMOTION:				
-				//playerRect.x = event.motion.x; 
-				//playerRect.y = event.motion.y;
-				playerTarget.x = event.motion.x - 50;
-				playerTarget.y = event.motion.y - 50;
+			case SDLK_w:
+				playerPosition.y -= 10;
+				break;
+			case SDLK_s:
+				playerPosition.y += 10;
+				break;
+			case SDLK_a:
+				playerPosition.x -= 10;
+				break;
+			case SDLK_d:
+				playerPosition.x += 10;
 				break;
 			case SDL_KEYDOWN:	if (event.key.keysym.sym == SDLK_ESCAPE) isRunning = false; break;
 			default:;
@@ -99,38 +103,59 @@ int main(int, char*[]) {
 		}
 
 		// UPDATE
-		//playerRect.x += (playerTarget.x - playerRect.x) / 10;
-		//playerRect.y += (playerTarget.y - playerRect.y) / 10;
-
 		deltaTime = (clock() - lastTime);
 		lastTime = clock();
 		deltaTime /= CLOCKS_PER_SEC;
 		timeDown -= deltaTime;
 		std::cout << timeDown << std::endl;
-
-
+		if (timeDown <= 0) timeDown = 10;
+		//playerRect.x += (playerTarget.x - playerRect.x) / 10;
+		//playerRect.y += (playerTarget.y - playerRect.y) / 10;
 		frameTime++;
 		if (FPS / frameTime <= 9)
 		{
 			frameTime = 0;
 			playerRect.x += frameWidth;
-			if (playerRect.x >= textWidth)
-				playerRect.x = 0;
+			if (playerRect.x > textWidth / 8)playerRect.x = 0;
+			//if (playerRect.x >= textWidth) playerRect.x = 0;
 		}
-
+		if (playerPosition.y <= 150)
+		{
+			playerPosition.y = 150;
+			if (playerPosition.x < 0)
+			{ 
+				playerPosition.x = 0;
+			}
+			else if (playerPosition.x > SCREEN_WIDTH - 30)
+			{
+				playerPosition.x = SCREEN_WIDTH - 30;
+			}
+		}
+		else if (playerPosition.y > SCREEN_HEIGHT - 35)
+		{
+			playerPosition.y = SCREEN_HEIGHT - 35;
+		}
+		else if (playerPosition.x > SCREEN_WIDTH - 30)
+		{
+			playerPosition.x = SCREEN_WIDTH - 30;
+		}
+		else if (playerPosition.x < 0)
+		{
+			playerPosition.x = 0;
+		}
 
 		// DRAW
 		SDL_RenderClear(renderer);
 		SDL_RenderCopy(renderer, bgTexture, nullptr, &bgRect);
 		SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
 		//SDL_RenderCopy(renderer, playerTexture, nullptr, &playerRect);
-		//SDL_RenderPresent(renderer);
-			//Background
-		
-			//Animated Sprite
-		
+		//Background
+
+		//Animated Sprite
 		SDL_RenderCopy(renderer, playerTexture, &playerRect, &playerPosition);
 		SDL_RenderPresent(renderer);
+
+
 	}
 
 	// --- DESTROY ---
